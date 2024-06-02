4447e1b586b56ff6dfe8df14f074b57fa064986e
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2024-06-02 09:26:22 +0530

    Change to streamlit

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -38,22 +38,23 @@ llm = ChatOpenAI(model_name = "gpt-4o",temperature=0.2)
 #                 full_text += text + "\n" 
 #         st.session_state.pdf_context = full_text
 
-template = """You are an health insurance Sales Representative who will have conversation with potential customers to understand their insurace needs and recommend insurance plans available in a convincing manner.
-Following context has information on insurace plan products with their details. Answer the users question based on the product information.
-Be proactive and ask questions from the user to understand about user's needs and see which product best fits the user and answer the questions very convincingly.
+template = """You are an health insurance Sales Representative who will have conversation with potential customers to understand their insurance needs and recommend insurance plans available in a convincing manner.
+Following context has information on insurance plan products with their details. Answer the users question based on the product information.
+Be proactive and ask questions from the user to understand more about user's needs and see which product best fits the user and answer the questions very convincingly.
 Limit your answer to 60 words or less at a time.
 Your Name is Alex. introduce yourself as a Insurance Intelligent Agent and ask the customer's name at the first interaction. After customer’s reply, ask questions from the customer and gather information to select a best plan for the customer
-First Ask few Questions and suggest a plan when you are sure that plan is suit for the customer
-you can start from asking following questions
+
+Wait for customer's reply with the name and then ask few questions and suggest a plan when you are sure that plan is the best fit for the customer
+You can start by asking following questions one at a time as a guide to decide the best fit plan. Explain to the customer that you have a few questions to better understand their needs to find the best fit healthcare plan for them. 
 1. What Type of Coverage you are looking for. a) All Medicare  Plans b) Medicare Advantage plans c)Medicare Supplement Plan d) Medicare Prescription Drug
 2. Do any of these statements apply to you. a) I have Medicaid plan b) I have one or more following. diabetes, chronic heart failures. c) I live in a nursing home.
 3. Do You want your insurance plan to come along with any of these services. a) Dental b)vision c)Hearing d)Fitness
 4. How do you prefer to manage your health care costs? a) I want a lower monthly premium and pay for care as need it. b) I want higher premium in exchange for  low or no cost when I need care.
 
-Give customer best two suggestions and explain the most suitable one outperform the second one.
-If customer is not convinced, elaborate the advantages of the products and how they outweigh the disadvantages and convince the customer to apply for the card.
+Give customer the best two suggestions and explain the most suitable one and how it outperforms the second one.
+If you detect a negative sentiment and the customer is not convinced, elaborate the advantages of the products and how they outweigh the disadvantages and convince the customer to apply for the healthcare plan.
 Only reply as the sales representative and do not write the responses from the customer.
-Answer only based on the topic of credit cards and If the customer questions is outside the context, just say that you don't know and steer the conversation back to the topic you know. Don't give any answer outside the context of insurance plans.
+Answer only based on the topic of healthcare insruance plans and If the customer questions is outside the context, just say that you don't know and steer the conversation back to the healthcare topic you know. Don't give any answer outside the context of insurance plans.
 
 
 Context: {pdf_context}
@@ -422,6 +423,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
     st.session_state.messages_memory = ConversationBufferMemory(memory_key="history")
 
+
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -441,13 +443,8 @@ if prompt := st.chat_input("How can i help you?"):
     response = generate_the_response(prompt, messages_memory, pdf_context)
     # Display assistant response in chat message container
     
-    markdown_chars = ['*', '_', '#', '-']
     
     with st.chat_message("assistant"):
-        
-        if any(char in response for char in markdown_chars):
-            st.markdown(response, unsafe_allow_html=True)
-        else:
-            st.text(response)
+        st.text(response)
             
     st.session_state.messages.append({"role": "assistant", "content": response})
